81e96fd05bde442ff67e2cd5d3101188028b52a1
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-04-08 22:43:50 +0530

    logi

--- logi/modules/alive.py
+++ logi/modules/alive.py
@@ -7,10 +7,10 @@ from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
 from logi.events import register
 from logi import telethn as tbot
-from logi import BOT_USERNAME ,OWNER_USERNAME
+from logi import BOT_USERNAME ,OWNER_USERNAME ,ALIVE_IMG ,SUPPORT_CHAT ,SUPPORT_CHANNEL
 
 
-PHOTO = "https://te.legra.ph/file/e8595050c5c4ea14f34d9.jpg"
+PHOTO = {ALIVE_IMG}
 
 @register(pattern=("/alive"))
 async def awake(event):
@@ -20,6 +20,7 @@ async def awake(event):
   TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += f"⚪ **My Repo :** [GitHub](https://github.com/LOGI-TECH/LOGI-BOT) \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/{BOT_USERNAME}?start=help"), Button.url("Support", "https://t.me/logi_channel")]]
+  BUTTON = [[Button.url("Update", "https://t.me/{SUPPORT_CHANNEL}"), Button.url("Support", "https://t.me/{SUPPORT_CHAT}")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
