fe197b3be945249c4ff1bb2e83ee9531013993d5
LOGI-LAB <LOGI-LAB@users.noreply.github.com>
2022-04-19 12:33:18 +0000

    Automated code formatting
    Signed-off-by: kennedy-ex <kennedy-ex@users.noreply.github.com>

--- logi/modules/alive.py
+++ logi/modules/alive.py
@@ -7,20 +7,26 @@ from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
 from logi.events import register
 from logi import telethn as tbot
-from logi import BOT_USERNAME ,OWNER_USERNAME ,SUPPORT_CHAT ,SUPPORT_CHANNEL
+from logi import BOT_USERNAME, OWNER_USERNAME, SUPPORT_CHAT, SUPPORT_CHANNEL
 
 
 PHOTO = "https://te.legra.ph/file/2fc8882813e32c8f90e08.jpg"
 
+
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm  Robot.** \n\n"
-  TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Master : [KING](https://t.me/{OWNER_USERNAME})** \n\n"
-  TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
-  TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += f"⚪ **My Repo :** [GitHub](https://github.com/LOGI-TECH/LOGI-BOT) \n\n"
-  TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Update", "https://t.me/{SUPPORT_CHANNEL}"), Button.url("Support", "https://t.me/{SUPPORT_CHAT}")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
+    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm  Robot.** \n\n"
+    TEXT += "⚪ **I'm Working Properly** \n\n"
+    TEXT += f"⚪ **My Master : [KING](https://t.me/{OWNER_USERNAME})** \n\n"
+    TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
+    TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
+    TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
+    TEXT += f"⚪ **My Repo :** [GitHub](https://github.com/LOGI-TECH/LOGI-BOT) \n\n"
+    TEXT += "**Thanks For Adding Me Here ❤️**"
+    BUTTON = [
+        [
+            Button.url("Update", "https://t.me/{SUPPORT_CHANNEL}"),
+            Button.url("Support", "https://t.me/{SUPPORT_CHAT}"),
+        ]
+    ]
+    await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
